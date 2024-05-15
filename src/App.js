172604ee3a43670f47b49cb0b9eb3d463359b944
hrd05 <harshdunkhwal55@gmail.com>
2024-05-15 17:46:58 +0530

    opening and closing cart using state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import Header from "./components/Layout/Header";
 import { Fragment } from "react";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+
+  const [cartOpen, setCart] = useState(false);
+
+  const openModalHandler = () => {
+    setCart(true);
+  }
+
+  const closeModalHandler = () => {
+    setCart(false);
+  }
+
   return (
     <Fragment>
-      <Header />
+      <Header onClick={openModalHandler} />
       <main>
-        <Cart />
+        {cartOpen && <Cart onClose={closeModalHandler} />}
         <Meals />
       </main>
     </Fragment>
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Fragment } from 'react';
 
 const Backdrop = (props) => {
-    return <div className={classes.backdrop} />
+    return <div onClick={props.onClose} className={classes.backdrop} />
 
 };
 
@@ -20,7 +20,7 @@ const portalElement = document.getElementById('overlays');
 const Modal = (props) => {
     return (
         <Fragment>
-            {ReactDOM.createPortal(<Backdrop />, portalElement)}
+            {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
             {ReactDOM.createPortal(<OverlayModal>{props.children}</OverlayModal>, portalElement)}
         </Fragment>
     )
